ea0393eebd6233c47a5b0225c0b83bb52b6aaf53
Potchara Puttawanchai <potchangelo@gmail.com>
2022-08-16 15:50:31 +0700

    Select and insert with DB

--- index.php
+++ index.php
@@ -1,15 +1,46 @@
 <?php require("utils/db.php"); ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Colorfull 09AF</title>
 </head>
+
 <body>
     <h2>Colorfull 09AF</h2>
-    <div>Output here</div>
-    <div><?php var_dump($connection); ?></div>
+    <?php
+    $searchValue = "";
+    if (isset($_GET["search"])) {
+        $searchValue = $_GET["search"];
+    }
+    ?>
+    <form>
+        <p>
+            <input type="search" name="search" value="<?php echo $searchValue; ?>" placeholder="ค้นหาจากชื่อสี">
+        </p>
+        <p>
+            <button type="submit">ค้นหา</button>
+        </p>
+    </form>
+    <?php
+    $sql = "SELECT * FROM color ORDER BY created_at DESC";
+    $result = mysqli_query($connection, $sql);
+    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $count_rows = count($rows);
+    ?>
+    <h3>พบสี <?php echo $count_rows; ?> รายการ</h3>
+    <?php foreach ($rows as $row): ?>
+        <div class="color-item" style="border-color: <?php echo $row["code"]; ?>;">
+            <h4><?php echo $row["title"]; ?></h4>
+            <p><?php echo $row["code"]; ?></p>
+        </div>
+    <?php endforeach; ?>
 </body>
+
 </html>
+
+<?php mysqli_close($connection); ?>
