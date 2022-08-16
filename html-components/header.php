75153645553622bdde4be228fd740d88c504466b
Potchara Puttawanchai <potchangelo@gmail.com>
2022-08-16 16:38:05 +0700

    Static CSS and HTML components

---
+++ html-components/header.php
@@ -0,0 +1,7 @@
+<header class="site-header">
+  <h2>Colorfull 09AF</h2>
+  <div>
+    <a href="./">หน้าแรก</a>
+    <a href="new-color.php">เพิ่มสีใหม่</a>
+  </div>
+</header>
--- index.php
+++ index.php
@@ -28,30 +28,30 @@ if (isset($_GET["search"])) {
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php require("html-components/head.php"); ?>
   <title><?php echo $searchTitle; ?>Colorfull 09AF</title>
 </head>
 
 <body>
-  <h2>Colorfull 09AF</h2>
-  <form>
-    <p>
-      <input type="search" name="search" value="<?php echo $searchValue; ?>" placeholder="ค้นหาจากชื่อสี">
-    </p>
-    <p>
-      <button type="submit">ค้นหา</button>
-    </p>
-  </form>
-  <?php $rows = getColors($connection); ?>
-  <h3>พบสี <?php echo count($rows); ?> รายการ</h3>
-  <?php foreach ($rows as $row) : ?>
-    <div class="color-item" style="border-color: <?php echo $row["code"]; ?>;">
-      <h4><?php echo $row["title"]; ?></h4>
-      <p><?php echo $row["code"]; ?></p>
+  <?php require("html-components/header.php"); ?>
+  <section class="section">
+    <div class="container">
+      <form>
+        <p>
+          <input type="search" name="search" value="<?php echo $searchValue; ?>" placeholder="ค้นหาจากชื่อสี">
+          <button type="submit">ค้นหา</button>
+        </p>
+      </form>
+      <?php $rows = getColors($connection); ?>
+      <h3>พบสี <?php echo count($rows); ?> รายการ</h3>
+      <?php foreach ($rows as $row) : ?>
+        <div class="color-item" style="border-color: <?php echo $row["code"]; ?>;">
+          <h4><?php echo $row["title"]; ?></h4>
+          <p><?php echo $row["code"]; ?></p>
+        </div>
+      <?php endforeach; ?>
     </div>
-  <?php endforeach; ?>
+  </section>
 </body>
 
 </html>
