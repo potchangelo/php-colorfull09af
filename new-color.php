f81320305fcfa0dd09cfe375ea9e7fa22e18b316
Potchara Puttawanchai <potchangelo@gmail.com>
2022-08-16 16:42:28 +0700

    Refractor

--- new-color.php
+++ new-color.php
@@ -13,42 +13,45 @@ function createColor($connection) {
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php require("html-components/head.php"); ?>
   <title>เพิ่มสีใหม่ | Colorfull 09AF</title>
 </head>
 
 <body>
-  <?php if ($_SERVER["REQUEST_METHOD"] == "POST") : ?>
-    <?php $result = createColor($connection); ?>
-    <?php if ($result) : ?>
-      <h2>เพิ่มสีใหม่เรียบร้อย</h2>
-      <p>
-        <a href="./">กลับหน้าแรก</a>
-      </p>
-    <?php else : ?>
-      <h2>เพิ่มสีใหม่ผิดพลาด</h2>
-      <p>
-        <a href="new-color.php">เพิ่มสีใหม่อีกครั้ง</a>
-      </p>
-    <?php endif; ?>
-  <?php else : ?>
-    <h2>เพิ่มสีใหม่</h2>
-    <form method="post">
-      <p>
-        <label>โค้ดสี</label>
-        <input type="color" name="code">
-      </p>
-      <p>
-        <label>ชื่อสี</label>
-        <input type="text" name="title">
-      </p>
-      <p>
-        <button type="submit">บันทึก</button>
-      </p>
-    </form>
-  <?php endif; ?>
+  <?php require("html-components/header.php"); ?>
+  <section class="section">
+    <div class="container">
+      <?php if ($_SERVER["REQUEST_METHOD"] == "POST") : ?>
+        <?php $result = createColor($connection); ?>
+        <?php if ($result) : ?>
+          <h3>เพิ่มสีใหม่เรียบร้อย</h3>
+          <p>
+            <a href="./">กลับหน้าแรก</a>
+          </p>
+        <?php else : ?>
+          <h2>เพิ่มสีใหม่ผิดพลาด</h2>
+          <p>
+            <a href="new-color.php">เพิ่มสีใหม่อีกครั้ง</a>
+          </p>
+        <?php endif; ?>
+      <?php else : ?>
+        <h3>เพิ่มสีใหม่</h3>
+        <form method="post">
+          <p>
+            <label>โค้ดสี</label>
+            <input type="color" name="code">
+          </p>
+          <p>
+            <label>ชื่อสี</label>
+            <input type="text" name="title">
+          </p>
+          <p>
+            <button type="submit">บันทึก</button>
+          </p>
+        </form>
+      <?php endif; ?>
+    </div>
+  </section>
 </body>
 
 </html>
