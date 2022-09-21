e30ddc2f82727f5375ab92da2ce8375733c59b9a
Potchara Puttawanchai <potchangelo@gmail.com>
2022-09-21 13:01:01 +0700

    Set mysqli_report mode to not throw error
    For PHP v.8.1 up
    https://php.watch/versions/8.1/mysqli-error-mode

--- utils/db.php
+++ utils/db.php
@@ -3,7 +3,8 @@
 require("utils/db-config.php");
 
 // Start connection
+mysqli_report(MYSQLI_REPORT_OFF);
 $connection = mysqli_connect($host, $username, $password, $dbname, $port);
 if (!$connection) {
-    die("DB connection error : " . mysqli_connect_error());
+  die("DB connection error : " . mysqli_connect_error());
 }
